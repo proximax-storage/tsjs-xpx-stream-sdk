bfce71620dab9ced178a2c422b96cd5b6512402a
Ruell <ruellm@yahoo.com>
2020-12-02 12:24:57 +0800

    remove use of webassembly in SDK as it should be handled in the client/frontend side.

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import {FlatCertificate} from "./src/cert/FlatCertificate";
 export {VideoStream, VideoStreamParameters} from "./src/routing/video/VideoStream";
 export * as BinaryHelper from "./src/utils/Binary";
 export {SiriusStreamClient} from "./src/client/SiriusStreamClient";
-export {high_resolution_clock as HighResolutionClock} from "./src/utils/HighResolutionClock";
 export {Frame, FrameType, Orientation} from "./src/media/Frame";
 export {SoundBuf, fProtocol, fCompSPEEX} from "./src/media/SoundBuf";
 export {SignedEd25519KeyPair, Ed25519KeyPair} from "./src/cert/KeyPair";
