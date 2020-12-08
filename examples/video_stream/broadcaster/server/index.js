bfff88aec3a8326bcf35bac75bbd849fd22f6fef
Ruell Magpayo <ruellm@yahoo.com>
2020-12-08 15:38:00 +0800

    corrected sample to use correct hostname

--- examples/video_stream/broadcaster/server/index.js
+++ examples/video_stream/broadcaster/server/index.js
@@ -24,13 +24,13 @@ const fCompSPEEX = require('tsjs-xpx-stream-sdk').fCompSPEEX;
 var config = {
     "bootstrap" : [
         {
-            "address": "discovery1:6001",
+            "address": "discovery1",
             "port": 6001,
             "identity": "xpx.discovery.node.6r7hhn9em5jT7FMX1XUdT3rmzvLR9anrfeCPwDy9CNnfvgUrR",
             "fingerprint": "39F55A3B00FB4E372553ABCB9763680EEF7A85CF49A12335B740C86D95CE7DBA",
         },
         {
-            "address": "discovery5:6005",
+            "address": "discovery5",
             "port": 6005,
             "identity": "xpx.discovery.node.9H8hXQ31pM9YWRBgi2ahn3X2hdSuVrWR78iJ2eo1SwWhktjgt",
             "fingerprint": "5AD8CEBB10D0521274FFC202A3D356DFD86453C0A12E2F02C2BE087A842CCD53",
